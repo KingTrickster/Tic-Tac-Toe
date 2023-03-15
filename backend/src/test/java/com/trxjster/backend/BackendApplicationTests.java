65334116533ebe3a754c2362c50dfe1c81f06de2
Alejandro Trejo <alextrejores@gmail.com>
2023-03-14 23:31:05 -0600

    first commit

---
+++ backend/src/test/java/com/trxjster/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trxjster.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
